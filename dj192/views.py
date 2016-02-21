2cc2481efe48e7e24668ab5ad1990d144f337726
axltsai <axl0326@gmail.com>
2016-02-21 14:35:25 +0800

    template 變量、標籤

--- dj192/views.py
+++ dj192/views.py
@@ -3,13 +3,7 @@
 #Created by axltsai on 2016/02/21 11:24
 
 from django.http import HttpResponse
-from django.template.loader import get_template
-from django import template
 from django.shortcuts import render_to_response
-
-def here(request):
-    return HttpResponse('hey,I am here! 中文測試！！')
-
 
 def math(request,a,b):
     a = int(a)
@@ -19,22 +13,25 @@ def math(request,a,b):
     p = a * b
     q = a / b
 
-    # html = '''<html>sum={s}<br>dif={d}<br>
-    #               pro={p}<br>quo={q}</html>'''.format(s=s,d=d,p=p,q=q)
-
-    # with open('templates/math.html','r') as reader:
-    #     t = template.Template(reader.read())
-
-    # t = get_template('math.html')
-    # c = template.Context({'s':s,
-    #                       'd':d,
-    #                       'p':p,
-    #                       'q':q,
-    #                       'a':a,
-    #                       'b':b,
-    #                       })
-    # return HttpResponse(t.render(c))
 
     # from django.shortcuts import render_to_response 做了:get_template(),render(),HttpResponse() 三合一
     # locals() 自動將所有變數做成dictionary
     return render_to_response('math.html',locals())
+
+def menu(request):
+    foods = [
+        {
+            'name' : '炒蛋',
+            'price' : 60,
+            'comment' : 'yummy',
+            'is_spicy' : False
+        },
+        {
+            'name' : '魯肉飯',
+            'price' : 50,
+            'comment' : '油多',
+            'is_spicy' : False
+        },
+    ]
+
+    return render_to_response('menu.html',locals())
