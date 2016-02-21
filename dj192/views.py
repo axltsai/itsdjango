3adcb8f86862594dee3cae475ef95c3fd3c07a61
axltsai <axl0326@gmail.com>
2016-02-21 13:13:24 +0800

    # from django.shortcuts import render_to_response 做了:get_template(),render(),HttpResponse() 三合一     # locals() 自動將所有變數做成dictionary

--- dj192/views.py
+++ dj192/views.py
@@ -5,6 +5,7 @@
 from django.http import HttpResponse
 from django.template.loader import get_template
 from django import template
+from django.shortcuts import render_to_response
 
 def here(request):
     return HttpResponse('hey,I am here! 中文測試！！')
@@ -17,17 +18,23 @@ def math(request,a,b):
     d = a - b
     p = a * b
     q = a / b
+
     # html = '''<html>sum={s}<br>dif={d}<br>
     #               pro={p}<br>quo={q}</html>'''.format(s=s,d=d,p=p,q=q)
 
     # with open('templates/math.html','r') as reader:
     #     t = template.Template(reader.read())
-    t = get_template('math.html')
-    c = template.Context({'s':s,
-                          'd':d,
-                          'p':p,
-                          'q':q,
-                          'a':a,
-                          'b':b,
-                          })
-    return HttpResponse(t.render(c))
+
+    # t = get_template('math.html')
+    # c = template.Context({'s':s,
+    #                       'd':d,
+    #                       'p':p,
+    #                       'q':q,
+    #                       'a':a,
+    #                       'b':b,
+    #                       })
+    # return HttpResponse(t.render(c))
+
+    # from django.shortcuts import render_to_response 做了:get_template(),render(),HttpResponse() 三合一
+    # locals() 自動將所有變數做成dictionary
+    return render_to_response('math.html',locals())
