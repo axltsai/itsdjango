0c6fc7eefb9420ee1145f1590085b22f9a7a223f
axltsai <axl0326@gmail.com>
2016-02-24 22:39:48 +0800

    Chapter 11.1 表單驗證

--- restaurants/views.py
+++ restaurants/views.py
@@ -28,18 +28,27 @@ def comment(request,id):
         r = Restaurant.objects.get(id=id)
     else:
         return HttpResponseRedirect("/restaurant_list/")
-
+    error = True
+    errors = []
     if request.POST:
         visitor = request.POST['visitor']
         content = request.POST['content']
         email = request.POST['email']
         date_time = timezone.localtime(timezone.now())
-        Comment.objects.create(
-            visitor = visitor,
-            email = email,
-            content = content,
-            date_time = date_time,
-            restaurant = r
-        )
+        # error = any(not request.POST[k] for k in request.POST)  #檢查每個欄位都要有值
+        if any(not request.POST[k] for k in request.POST):
+            errors.append('* 有空白欄位,請不要留空')
+        if '@' not in email:
+            # error = True
+            errors.append('* email 格式不正確,請重新輸入')
+        if not errors:
+            Comment.objects.create(
+                visitor = visitor,
+                email = email,
+                content = content,
+                date_time = date_time,
+                restaurant = r
+            )
+            visitor,email,content = ('','','')
     return render_to_response('comments.html',
                               RequestContext(request,locals()))
